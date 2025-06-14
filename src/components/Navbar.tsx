14b2cbdb1e30c4dfc8018d0a739dc60382a5d90a
prayush21 <prayushdave@gmail.com>
2025-06-14 17:53:06 -0400

    Update: Navbar for Mobile & Socials Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,13 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, onToggleTheme }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="fixed my-2 w-11/12 top-4 left-1/2 -translate-x-1/2 shadow-neutral-500 shadow-md z-10 backdrop-blur border-t-2 border-t-neutral-100 rounded-xl  bg-lightBackground/0 dark:bg-darkBackground/0">
+    <header
+      className={`fixed my-2 w-11/12 top-4 left-1/2 -translate-x-1/2 z-10 backdrop-blur border-t-2 border-t-neutral-100 rounded-xl bg-light-background/0 dark:bg-darkBackground/0 transition-all duration-300 ${
+        open
+          ? " border-b-2 border-b-neutral-500 shadow-md"
+          : "shadow-neutral-500 shadow-md"
+      }`}
+    >
       <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
         {/* Desktop Logo/Name and Theme Toggle */}
         <div className="hidden md:flex items-center space-x-4">
@@ -55,9 +61,9 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, onToggleTheme }) => {
           onClick={() => setOpen(!open)}
         >
           {open ? (
-            <XMarkIcon className="h-6 w-6 text-lightTextPrimary dark:text-darkTextPrimary" />
+            <XMarkIcon className="h-6 w-6 text-light-textPrimary dark:text-dark-textPrimary" />
           ) : (
-            <Bars3Icon className="h-6 w-6 text-lightTextPrimary dark:text-darkTextPrimary" />
+            <Bars3Icon className="h-6 w-6 text-light-textPrimary dark:text-dark-textPrimary" />
           )}
         </button>
         <img
@@ -69,13 +75,15 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, onToggleTheme }) => {
 
       {/* Mobile Drawer */}
       <div
-        className={`fixed top-0 left-0 min-h-screen w-full bg-light-background/95 dark:bg-dark-background/95 transform transition-all duration-300 ease-in-out ${
-          open ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"
-        } md:hidden z-20 shadow-lg rounded-xl backdrop-blur-sm`}
+        className={`absolute top-full left-0 w-full bg-light-background/95 dark:bg-dark-background/95 transform transition-all duration-300 ease-in-out ${
+          open
+            ? "translate-y-0 opacity-100"
+            : "-translate-y-4 opacity-0 pointer-events-none"
+        } md:hidden z-20 shadow-lg rounded-xl backdrop-blur-sm mt-2`}
       >
-        <div className="flex flex-col justify-normal gap-4 rounded-xl min-h-screen  pt-4 pb-10 px-4">
+        <div className="flex flex-col justify-normal gap-8 rounded-xl pt-4 pb-10 px-4">
           {/* Top section with Logo */}
-          <div className="flex items-start justify-between">
+          {/* <div className="flex items-start justify-between">
             <button
               onClick={() => setOpen(false)}
               className="p-2 rounded-md bg-lightBackground/80 dark:bg-darkBackground/80 hover:bg-lightBackground/90 dark:hover:bg-darkBackground/90 transition-colors duration-200"
@@ -95,7 +103,7 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, onToggleTheme }) => {
               </button>
               <img src={logoName} alt="Prayush Dave" className="h-12 w-auto" />
             </div>
-          </div>
+          </div> */}
 
           {/* Main Section with Navigation Links in the Middle for Mobile Screens */}
           <div className="flex items-end justify-between">
@@ -111,13 +119,40 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, onToggleTheme }) => {
               ))}
             </ul>
           </div>
+          <div className="flex gap-2 w-full justify-around">
+            <a href="https://x.com/prayushdave">
+              <svg className="icon">
+                <use xlinkHref="#icon-x" />
+              </svg>
+            </a>
+            <a href="https://github.com/prayush21">
+              <svg className="icon">
+                <use xlinkHref="#icon-github" />
+              </svg>
+            </a>
+            <a href="https://www.instagram.com/prayushdave">
+              <svg className="icon">
+                <use xlinkHref="#icon-instagram" />
+              </svg>
+            </a>
+            <a href="https://www.linkedin.com/in/prayush-dave/">
+              <svg className="icon">
+                <use xlinkHref="#icon-linkedin" />
+              </svg>
+            </a>
+            <a href="https://www.youtube.com/@prayushdave">
+              <svg className="icon">
+                <use xlinkHref="#icon-youtube" />
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
 
       {/* Overlay */}
       {open && (
         <div
-          className="fixed shadow-white shadow-md rounded-xl bg-light-background dark:bg-dark-card h-svh inset-0 md:hidden z-10 transition-opacity duration-300"
+          className="fixed inset-0 md:hidden z-10 transition-opacity duration-300"
           onClick={() => setOpen(false)}
         />
       )}
