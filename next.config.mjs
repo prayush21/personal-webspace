879efa4ad984fa813f83523370fa2f861ecf7279
prayush21 <prayushdave@gmail.com>
2025-07-18 09:54:19 -0400

    Fix: CSS and Asset Rendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/personal-webspace",
+  assetPrefix: "/personal-webspace/",
   trailingSlash: true,
   images: {
     unoptimized: true,
