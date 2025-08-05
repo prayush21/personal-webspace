ba30241feb67b8f5688675e56929b40f6352dc2c
prayush21 <prayushdave@gmail.com>
2025-08-05 01:17:41 -0400

    Update Projects Asset Path

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,3 +1,5 @@
+import { getAssetPath } from "./paths";
+
 export interface Project {
   id: string;
   title: string;
@@ -11,7 +13,7 @@ export const projects: Project[] = [
   {
     id: "1",
     title: "Connect - a collaborative word game",
-    screenshotSrc: "/assets/connect-screenshot.png",
+    screenshotSrc: getAssetPath("/assets/connect-screenshot.png"),
     demoUrl: "https://connect-38fe1.web.app/",
     repoUrl: "https://github.com/prayush21/connect-a-fun-social-game",
     tags: ["Next.js", "TypeScript", "Tailwind CSS"],
@@ -19,15 +21,14 @@ export const projects: Project[] = [
   {
     id: "2",
     title: "Cricket Era Dashboard",
-    screenshotSrc: "/assets/cricket-tera-dashboard.png",
-    // demoUrl: "https://example.com",
+    screenshotSrc: getAssetPath("/assets/cricket-tera-dashboard.png"),
     repoUrl: "https://github.com/prayush21/cricket-tera",
     tags: ["Python", "Data Analysis & Visualization", "PostgresDB"],
   },
   {
     id: "4",
     title: "Wordle for Mom",
-    screenshotSrc: "/assets/wordle-clone-image.png",
+    screenshotSrc: getAssetPath("/assets/wordle-clone-image.png"),
     demoUrl: "https://wordleforreal.netlify.app/",
     repoUrl: "https://github.com/prayush21/wordle-react",
     tags: ["Creative Coding", "Typescript", "React"],
@@ -35,7 +36,7 @@ export const projects: Project[] = [
   {
     id: "3",
     title: "Weather Dashboard",
-    screenshotSrc: "/assets/weather-app-ss.png",
+    screenshotSrc: getAssetPath("/assets/weather-app-ss.png"),
     demoUrl: "https://weather-app-zeta-silk-68.vercel.app/",
     repoUrl: "https://github.com/prayush21/weather-app",
     tags: ["React", "Redux", "OpenWeather API"],
