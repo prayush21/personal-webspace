cb70d0059546579c4f64161948d7f36cd8c18ca9
prayush21 <prayushdave@gmail.com>
2026-01-29 22:27:58 -0500

    feat: add new projects with screenshots for Netflix-like Video Player and Feedback Pulse

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -28,13 +28,29 @@ export const projects: Project[] = [
   },
   {
     id: "3",
+    title: "Netflix-like Video Player Chrome Extension",
+    screenshotSrc: getAssetPath("/assets/netflix-chrome-extension.png"),
+    demoUrl:
+      "https://chromewebstore.google.com/detail/hglkjjlgpdiaohggjagghfnmihmdljoi?utm_source=item-share-cb",
+    repoUrl: "https://github.com/prayush21/video-player-chrome-extension",
+  },
+  {
+    id: "4",
+    title: "Feedback Pulse - Product Feedback Agent",
+    screenshotSrc: getAssetPath("/assets/feedback-pulse-screenshot.png"),
+    demoUrl: "https://feedback-pulse.prayushdave.workers.dev/",
+    repoUrl: "https://github.com/prayush21/product-feedback-agent",
+  },
+
+  {
+    id: "5",
     title: "Cricket D3 (Dismissal Distribution Dashboard)",
     screenshotSrc: getAssetPath("/assets/cricket-tera-dashboard.png"),
     repoUrl: "https://github.com/prayush21/cricket-tera",
     tags: ["Python", "Data Analysis & Visualization", "PostgresDB"],
   },
   {
-    id: "4",
+    id: "6",
     title: "Wordle for Mom",
     screenshotSrc: getAssetPath("/assets/wordle-clone-image.png"),
     demoUrl: "https://wordleforreal.netlify.app/",
@@ -42,7 +58,7 @@ export const projects: Project[] = [
     tags: ["Creative Coding", "Typescript", "React"],
   },
   {
-    id: "5",
+    id: "7",
     title: "Weather Dashboard",
     screenshotSrc: getAssetPath("/assets/weather-app-ss.png"),
     demoUrl: "https://weather-app-zeta-silk-68.vercel.app/",
